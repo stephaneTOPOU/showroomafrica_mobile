8c99875893631e7c41e11a22f1cb6657a364b980
Vivien <vivientopou57@gmail.com>
2023-08-01 15:04:13 +0000

    commit

--- app/src/main/java/com/showroomafrica/annuaire/MainActivity.java
+++ app/src/main/java/com/showroomafrica/annuaire/MainActivity.java
@@ -1,6 +1,9 @@
 package com.showroomafrica.annuaire;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
@@ -14,6 +17,15 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        WindowInsetsControllerCompat windowInsetsController =
+                ViewCompat.getWindowInsetsController(getWindow().getDecorView());
+        if (windowInsetsController == null) {
+            return;
+        }
+
+        //windowInsetsController.setAppearanceLightNavigationBars(true);
         setContentView(R.layout.activity_main);
 
 
